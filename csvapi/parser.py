cef020b245610584bef620e8782478545c79fb03
Alexandre Bulté <alexandre@bulte.net>
2018-10-03 17:47:43 +0200

    Invert try order

--- csvapi/parser.py
+++ csvapi/parser.py
@@ -22,9 +22,9 @@ def detect_encoding(filepath):
 def from_csv(filepath, encoding='utf-8', sniff_limit=SNIFF_LIMIT):
     """Try first w/o sniffing and then sniff if it fails"""
     try:
-        return agate.Table.from_csv(filepath, encoding=encoding)
-    except ValueError:
         return agate.Table.from_csv(filepath, sniff_limit=sniff_limit, encoding=encoding)
+    except ValueError:
+        return agate.Table.from_csv(filepath, encoding=encoding)
 
 
 def from_excel(filepath):
