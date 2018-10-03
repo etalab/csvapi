f0984b78bd9dc40277923d1f6bdb07dd5ab23478
Alexandre Bulté <alexandre@bulte.net>
2018-10-03 17:42:05 +0200

    Try parsing CSV w/o sniffing first

--- csvapi/parser.py
+++ csvapi/parser.py
@@ -20,7 +20,11 @@ def detect_encoding(filepath):
 
 
 def from_csv(filepath, encoding='utf-8', sniff_limit=SNIFF_LIMIT):
-    return agate.Table.from_csv(filepath, sniff_limit=sniff_limit, encoding=encoding)
+    """Try first w/o sniffing and then sniff if it fails"""
+    try:
+        return agate.Table.from_csv(filepath, encoding=encoding)
+    except ValueError:
+        return agate.Table.from_csv(filepath, sniff_limit=sniff_limit, encoding=encoding)
 
 
 def from_excel(filepath):
