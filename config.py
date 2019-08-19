981b330f3fe5dd004aced42eea81bd896d5a2d6f
Alexandre Bulté <alexandre@bulte.net>
2019-08-19 12:04:47 +0200

    Raise sniff limit and add real life tests

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DEBUG = True
 SENTRY_DSN = None
 FORCE_SSL = False
 # In bytes, cf `sniff_limit` https://agate.readthedocs.io/en/1.6.1/api/table.html#agate.Table.from_csv
-CSV_SNIFF_LIMIT = 4096
+CSV_SNIFF_LIMIT = 4096 * 2
 # In bytes, csvapi will stop downloading files if they reach this size
 # Default to 100 Mo
 MAX_FILE_SIZE = 1024 * 1024 * 100
