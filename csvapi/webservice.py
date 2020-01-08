e498929956fbcc5cb987ccaa97634176ba955bfa
Alexandre Bulté <alexandre@bulte.net>
2020-01-08 22:29:50 +0100

    Add export view (export to CSV)

--- csvapi/webservice.py
+++ csvapi/webservice.py
@@ -7,11 +7,13 @@ from quart.exceptions import NotFound
 from csvapi.crossdomain import add_cors_headers
 from csvapi.errors import APIError
 from csvapi.tableview import TableView
+from csvapi.exportview import ExportView
 from csvapi.parseview import ParseView
 from csvapi.security import filter_referrers
 
 app = Quart(__name__)
 app.add_url_rule('/api/<urlhash>', view_func=TableView.as_view('table'))
+app.add_url_rule('/api/<urlhash>/export', view_func=ExportView.as_view('export'))
 app.add_url_rule('/apify', view_func=ParseView.as_view('parse'))
 app.after_request(add_cors_headers)
 app.before_request(filter_referrers)
