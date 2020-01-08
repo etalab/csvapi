fee73e6ae2a9b8560d0c4c1c9cac6c7c655267de
Alexandre Bulté <alexandre@bulte.net>
2020-01-09 00:10:05 +0100

    handle cache

--- csvapi/uploadview.py
+++ csvapi/uploadview.py
@@ -4,7 +4,7 @@ from tempfile import NamedTemporaryFile
 from quart import request, current_app as app, jsonify
 from quart.views import MethodView
 
-from csvapi.utils import get_hash_bytes
+from csvapi.utils import get_hash_bytes, already_exists
 from csvapi.parser import parse
 
 
@@ -21,15 +21,16 @@ class UploadView(MethodView):
                 tmpfile.write(data)
             tmpfile.close()
             with open(tmpfile.name, 'rb') as _tmpfile:
-                _hash = get_hash_bytes(_tmpfile.read())
-            storage = app.config['DB_ROOT_DIR']
-            sniff_limit = app.config.get('CSV_SNIFF_LIMIT')
-            parse(tmpfile.name, _hash, storage, sniff_limit=sniff_limit)
+                content_hash = get_hash_bytes(_tmpfile.read())
+            if not already_exists(content_hash):
+                storage = app.config['DB_ROOT_DIR']
+                sniff_limit = app.config.get('CSV_SNIFF_LIMIT')
+                parse(tmpfile.name, content_hash, storage, sniff_limit=sniff_limit)
         finally:
             os.unlink(tmpfile.name)
 
         scheme = 'https' if app.config.get('FORCE_SSL') else request.scheme
         return jsonify({
             'ok': True,
-            'endpoint': f"{scheme}://{request.host}/api/{_hash}"
+            'endpoint': f"{scheme}://{request.host}/api/{content_hash}"
         })
