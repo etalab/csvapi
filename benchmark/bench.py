a3b9f319ea9504f4449979a3ee7548dc1b410ae1
Alexandre Bulté <alexandre@bulte.net>
2022-11-03 15:11:46 +0100

    move and fill benchmark

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -1,7 +1,39 @@
+"""
+2022-11-03
+
+csvapi cli, no cache, pool_size=1, analysis=false
+-------------->Time execution : 17.989488124847412<--------------
+-------------->Time execution : 24.22131085395813<--------------
+-------------->Time execution : 21.933547019958496<--------------
+
+csvapi cli, no cache, pool_size=1, analysis=yes
+-------------->Time execution : 28.727387189865112<--------------
+-------------->Time execution : 27.748358964920044<--------------
+-------------->Time execution : 22.15376091003418<--------------
+
+csvapi cli, no cache, pool_size=0, analysis=yes
+-------------->Time execution : 27.46714496612549<--------------
+-------------->Time execution : 28.398924112319946<--------------
+-------------->Time execution : 25.25711679458618<--------------
+
+hypercorn -w3, no cache, pool_size=1, analysis=yes
+-------------->Time execution : 17.33577609062195<--------------
+-------------->Time execution : 27.747673988342285<--------------
+-------------->Time execution : 19.758486032485962<--------------
+
+hypercorn -w3, no cache, pool_size=0, analysis=yes
+-------------->Time execution : 23.761262893676758<--------------
+-------------->Time execution : 18.91990613937378<--------------
+-------------->Time execution : 31.557281017303467<--------------
+-------------->Time execution : 31.700807809829712<--------------
+-------------->Time execution : 32.8078031539917<--------------
+"""
+
 import aiohttp
 import asyncio
 import time
 
+ANALYSIS = True
 
 URLS_APIFY = [
     'http://localhost:8001/apify?url=http://datanova.legroupe.laposte.fr/explore/dataset/laposte_poincont2/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true',
@@ -14,6 +46,8 @@ URLS_APIFY = [
 
 
 async def fetch_apify(session, url):
+    if ANALYSIS:
+        url = url.replace('apify?', 'apify?analysis=yes&')
     async with session.get(url) as response:
         res = await response.json()
         return res['endpoint']
@@ -31,7 +65,7 @@ async def main():
         endpoints = await asyncio.gather(*apify_requests)
         api_requests = list()
         for endpoint in endpoints:
-            for x in range(20):
+            for _ in range(20):
                 api_requests.append(asyncio.ensure_future(fetch_api(session, endpoint)))
         await asyncio.gather(*api_requests)
     end = time.time()
